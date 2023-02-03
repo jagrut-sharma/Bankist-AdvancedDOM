a2d01b4c11c7e33312a3a1b46537dbace59257b8
Jagrut Sharma <jagrut.sharma1@gmail.com>
2023-02-03 11:55:32 +0530

    ways of adding style, attributes and classes

--- script.js
+++ script.js
@@ -63,8 +63,8 @@ message.classList.add('cookie-message');
 // message.innerText = `We use cookies`;
 message.innerHTML = `We use cookies for better speed and analytics. <button class="btn btn--close--cookie">Got it!</button>`;
 
-header.prepend(message); //  inside header before content
-// header.append(message); // inside header after content
+// header.prepend(message); //  inside header before content
+header.append(message); // inside header after content
 // header.append(message.cloneNode(true)); //  creates a clone and inserts it after content inside header
 // header.before(message); // inserts before header
 // header.after(message); // inserts after header
@@ -76,3 +76,46 @@ document
     // message.remove(); // New syntax
     message.parentElement.removeChild(message); // Selects parent element then removes child of parentElement --> Was used previously
   });
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '105%';
+
+console.log(message.style.backgroundColor); // rgb(55, 56, 61)
+console.log(message.style.color); // Wil just give a blank line as there is no inline CSS property od the same name
+
+// to get property use getComputedStyle method
+console.log(getComputedStyle(message)); // will give an object with properties that we didn't define but browser calculated for display
+console.log(getComputedStyle(message).height); // 43.6667px --> Getting value from object
+console.log(getComputedStyle(message).color); // rgb(187, 187, 187)
+
+// Can be used to keep height as well
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 25 + 'px';
+
+console.log(getComputedStyle(message).height);
+
+// Changing value of variable:
+document.documentElement.style.setProperty('--color-primary', 'yellow');
+
+// ATTRIBUTES:
+const logo = document.querySelector('.nav__logo');
+
+console.log(logo.alt); // Bankist logo
+console.log(logo.src); // http://127.0.0.1:5500/img/logo.png --> Gives absolute link
+console.log(logo.className);
+
+// Non-standard way
+console.log(logo.designer); // gives undefined as img tag is not supposed tp have designer attribute
+
+// Standard way
+console.log(logo.getAttribute('designer')); // Jagrut
+
+logo.alt = 'Minimalist logo of Bankist'; // will change the alt attribute of logo
+logo.setAttribute('company', 'bankist'); // will set an attribute of company
+
+console.log(logo.getAttribute('src')); // gives relative link => img/logo.png
+
+// Data Attributes:
+console.log(logo.dataset.versionNumber); // data- attributes will be obtained in dataset object
+
+// You can also use classList.add, remove, toggle to add/remove class. Can use contains to check.
